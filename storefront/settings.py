77a83cbb1f3679186742bc6226383abda57d7da2
Mav <riherr00@gmail.com>
2025-12-18 20:37:17 +0300

    2 new app parts created

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground.apps.PlaygroundConfig',
-    # 'playground',
+    'debug_toolbar',
+    'store.apps.StoreConfig',
+    'tags.apps.TagsConfig'
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'storefront.urls'
