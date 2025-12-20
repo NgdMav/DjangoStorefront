dec52d4436d2021810844454e0ec2201ef5fceb4
Mav <riherr00@gmail.com>
2025-12-20 17:32:28 +0300

    admin panel start

--- playground/views.py
+++ playground/views.py
@@ -3,10 +3,4 @@ from store.models import Product, Collection
 
 
 def say_hello(request):
-    collection = Collection()
-    collection.title = "Video Games"
-    collection.featured_product = Product(pk=1)
-    collection.save()
-
-    Collection.objects.create(title='a', featured_product_id=2)
     return render(request, 'hello.html', {"name": "Mav"})
