ac3c5b4d3a61739287532bc7422fa4d0f2751ca5
Mav <riherr00@gmail.com>
2025-12-20 13:59:44 +0300

    Creating objects

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from store.models import Product, Collection
+
 
 def say_hello(request):
+    collection = Collection()
+    collection.title = "Video Games"
+    collection.featured_product = Product(pk=1)
+    collection.save()
+
+    Collection.objects.create(title='a', featured_product_id=2)
     return render(request, 'hello.html', {"name": "Mav"})
