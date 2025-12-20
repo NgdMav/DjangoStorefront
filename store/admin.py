1613f39cacef71c7fc53d344025fd76914aa2c67
Mav <riherr00@gmail.com>
2025-12-20 18:08:05 +0300

    added a new column with new generated field

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+from django.db.models import Count
+
 from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -13,11 +16,20 @@ class ProductAdmin(admin.ModelAdmin):
             return 'Low'
         return 'Medium'
 
+    @admin.display(ordering='collection')
+    def collection_title(self, product):
+        return product.collection.title
+
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['customer', 'payment_status']
+    list_display = ['id', 'placed_at', 'customer_name', 'payment_status']
     list_editable = ['payment_status']
     list_per_page = 10
+    list_select_related = ['customer']
+
+    @admin.display(ordering='customer')
+    def customer_name(self, order):
+        return order.customer.first_name + ' ' + order.customer.last_name
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
@@ -27,6 +39,15 @@ class CustomerAdmin(admin.ModelAdmin):
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ['title', 'featured_product']
+    list_display = ['title', 'featured_product', 'products_count']
     list_editable = ['featured_product']
     list_per_page = 10
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        return collection.products_count
+
+    def get_queryset(self, request):
+        return (super()
+                .get_queryset(request)
+                .annotate(products_count=Count('product'))
+                )
