74409de120925d3365ecda69c86ab7cfdc841356
Mav <riherr00@gmail.com>
2025-12-20 17:45:29 +0300

    New bases added to admin

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Collection)
-admin.site.register(models.Product)
-admin.site.register(models.Order)
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+    list_per_page = 10
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'payment_status']
+    list_editable = ['payment_status']
+    list_per_page = 10
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'featured_product']
+    list_editable = ['featured_product']
+    list_per_page = 10
