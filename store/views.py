affe2ffff664ab9a8399ed6789986f7a1ff6df62
Mav <riherr00@gmail.com>
2025-12-21 11:13:03 +0300

    Serializer added. Response with actual data

--- store/views.py
+++ store/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from store.models import Product
+from store.serializer import ProductSerializer
 
 @api_view()
 def product_list(request):
-    products = Product.objects.all()
-    return Response('products')
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    # try:
+    #     product = Product.objects.get(id=id)
+    #     serializer = ProductSerializer(product, many=False)
+    #     return Response(serializer.data)
+    # except Product.DoesNotExist:
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
+
+    product = get_object_or_404(Product, id=id)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
