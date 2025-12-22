15e28805fc3662166e96202d0542c08d096da754
Mav <riherr00@gmail.com>
2025-12-22 17:47:27 +0300

    searching and sorting data added

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from store.filters import ProductFilter
 from store.models import Product, Collection, OrderItem, Review
@@ -12,8 +13,10 @@ from store.serializer import ProductSerializer, CollectionSerializer, ReviewSeri
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    search_fields = ['title', 'description']
+    ordering_fields = ['id', 'title', 'unit_price', 'inventory']
 
     def get_serializer_context(self):
         return {'request': self.request}
