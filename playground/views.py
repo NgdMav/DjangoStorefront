ed238cd7e183a7e5d2b8851a75c7a863419659a8
Mav <riherr00@gmail.com>
2025-12-24 16:08:52 +0300

    Sending simple emails

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 from store.models import Product, Collection
 
 
 def say_hello(request):
+    try:
+        send_mail('subject',
+                  'message',
+                  'info@mav.com',
+                  ['xivalor.gmail.com'])
+        mail_admins('admin_subject',
+                    'admin_message',
+                    html_message='<p>Hello Admin!</p>')
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {"name": "Mav"})
--- storefront/settings.py
+++ storefront/settings.py
@@ -168,3 +168,15 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = 'from@mav.com'
+
+ADMINS = [
+    ('Mav', 'mav@gmail.com'),
+    ('admin', 'admin@domain.com')
+]
