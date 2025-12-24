ce8b33a63508a636de1173ab61b5715b4db7d6ce
Mav <riherr00@gmail.com>
2025-12-24 12:28:46 +0300

    My own signal on creating a order

--- store/serializer.py
+++ store/serializer.py
@@ -3,6 +3,7 @@ from django.db import transaction
 
 from rest_framework import serializers
 from store.models import Product, Collection, Review, Cart, CartItem, Customer, Order, OrderItem
+from store.signals import order_created
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -152,6 +153,8 @@ class CreateOrderSerializer(serializers.Serializer):
             OrderItem.objects.bulk_create(order_items)
 
             Cart.objects.filter(pk=cart_id).delete()
+
+            order_created.send_robust(self.__class__, order=order)
 
             return order
 
