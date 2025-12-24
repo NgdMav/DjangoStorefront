24c835500861479ff2e8daa412369cdf5ae79148
Mav <riherr00@gmail.com>
2025-12-24 16:15:51 +0300

    Send emails with attach

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import EmailMessage, BadHeaderError
 from store.models import Product, Collection
 
 
 def say_hello(request):
     try:
-        send_mail('subject',
-                  'message',
-                  'info@mav.com',
-                  ['xivalor.gmail.com'])
-        mail_admins('admin_subject',
-                    'admin_message',
-                    html_message='<p>Hello Admin!</p>')
+        # send_mail('subject',
+        #           'message',
+        #           'info@mav.com',
+        #           ['xivalor.gmail.com'])
+        # mail_admins('admin_subject',
+        #             'admin_message',
+        #             html_message='<p>Hello Admin!</p>')
+        message = EmailMessage('subject',
+                     'message',
+                     'from@mail.com',
+                     ['xivalor@mail.com'])
+        message.attach_file('playground/static/images/vinland.png')
+        message.send()
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {"name": "Mav"})
