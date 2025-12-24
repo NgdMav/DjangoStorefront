8c40604294fcc8600b0372161aeb7f522ad706e3
Mav <riherr00@gmail.com>
2025-12-24 16:26:51 +0300

    templated emails

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.core.mail import EmailMessage, BadHeaderError
+from django.core.mail import BadHeaderError
+from templated_mail.mail import BaseEmailMessage
 from store.models import Product, Collection
 
 
@@ -12,12 +13,17 @@ def say_hello(request):
         # mail_admins('admin_subject',
         #             'admin_message',
         #             html_message='<p>Hello Admin!</p>')
-        message = EmailMessage('subject',
-                     'message',
-                     'from@mail.com',
-                     ['xivalor@mail.com'])
-        message.attach_file('playground/static/images/vinland.png')
-        message.send()
+        # message = EmailMessage('subject',
+        #              'message',
+        #              'from@mail.com',
+        #              ['xivalor@mail.com'])
+        # message.attach_file('playground/static/images/vinland.png')
+        # message.send()
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name': 'Mav'},
+        )
+        message.send(['xivalor@domain.com'])
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {"name": "Mav"})
