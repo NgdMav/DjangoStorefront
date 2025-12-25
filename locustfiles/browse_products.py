3f5c5e91d8907f4cf715c7bc5fa6ac3991d26683
Mav <riherr00@gmail.com>
2025-12-25 16:32:40 +0300

    Test performance testing with locust

---
+++ locustfiles/browse_products.py
@@ -0,0 +1,37 @@
+from locust import HttpUser, task, between
+from random import randint
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 5)
+    cart_id = None
+
+    def on_start(self) -> None:
+        response = self.client.post('/store/carts/')
+        result = response.json()
+        self.cart_id = result['id']
+
+    @task(2)
+    def view_products(self):
+        print('viewing products')
+        collection_id = str(randint(1, 10))
+        self.client.get(
+            f'/store/products/?collection={collection_id}',
+            name='/store/products')
+
+    @task(4)
+    def view_product(self):
+        print('viewing product details')
+        product_id = str(randint(1, 500))
+        self.client.get(
+            f'/store/products/{product_id}',
+            name='/store/products/:id')
+
+    @task(1)
+    def add_to_cart(self):
+        print('adding product to cart')
+        product_id = str(randint(1, 10))
+        self.client.post(
+            f'/store/carts/{self.cart_id}/items/',
+            name='/store/carts/items',
+            json={'product_id': product_id,
+                  'quantity': 1})
