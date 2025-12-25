2d39e79694242bfef3f4c6f19871743e5916e54f
Mav <riherr00@gmail.com>
2025-12-25 10:36:30 +0300

    First task for 10 sec in back

--- playground/views.py
+++ playground/views.py
@@ -1,29 +1,27 @@
 from django.shortcuts import render
-from django.core.mail import BadHeaderError
-from templated_mail.mail import BaseEmailMessage
-from store.models import Product, Collection
-
+from .tasks import notify_customers
 
 def say_hello(request):
-    try:
-        # send_mail('subject',
-        #           'message',
-        #           'info@mav.com',
-        #           ['xivalor.gmail.com'])
-        # mail_admins('admin_subject',
-        #             'admin_message',
-        #             html_message='<p>Hello Admin!</p>')
-        # message = EmailMessage('subject',
-        #              'message',
-        #              'from@mail.com',
-        #              ['xivalor@mail.com'])
-        # message.attach_file('playground/static/images/vinland.png')
-        # message.send()
-        message = BaseEmailMessage(
-            template_name='emails/hello.html',
-            context={'name': 'Mav'},
-        )
-        message.send(['xivalor@domain.com'])
-    except BadHeaderError:
-        pass
+    # try:
+    #     # send_mail('subject',
+    #     #           'message',
+    #     #           'info@mav.com',
+    #     #           ['xivalor.gmail.com'])
+    #     # mail_admins('admin_subject',
+    #     #             'admin_message',
+    #     #             html_message='<p>Hello Admin!</p>')
+    #     # message = EmailMessage('subject',
+    #     #              'message',
+    #     #              'from@mail.com',
+    #     #              ['xivalor@mail.com'])
+    #     # message.attach_file('playground/static/images/vinland.png')
+    #     # message.send()
+    #     message = BaseEmailMessage(
+    #         template_name='emails/hello.html',
+    #         context={'name': 'Mav'},
+    #     )
+    #     message.send(['xivalor@domain.com'])
+    # except BadHeaderError:
+    #     pass
+    notify_customers.delay('Hello, celery')
     return render(request, 'hello.html', {"name": "Mav"})
